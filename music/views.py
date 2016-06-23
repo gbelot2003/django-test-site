a26560ff828f8a626b8e20f043b7d3fc544ed086
gbelot <gbelot2003@hotmail.com>
2016-06-23 01:26:17 -0600

    creating detail.html and refactoring music.views adding try Catch to detail method avoiding not existent albums

--- music/views.py
+++ music/views.py
@@ -1,14 +1,16 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from .models import Album
 
 
 def index(request):
 	all_albums = Album.objects.all()
-	context = {
-		'all_albums': all_albums
-	}
-	return render(request, 'music/index.html', context)
+	return render(request, 'music/index.html', {'all_albums': all_albums})
 
 def detail(request, album_id):
-	return HttpResponse("<h2>Details for Album id: " +  str(album_id) + "</h2>")
+	try:
+		album = Album.objects.filter(id=album_id)
+	except Album.DoesNotExist:
+		raise Http404("No existe ese album")
+
+	return render(request, 'music/detail.html', {'album': album})
