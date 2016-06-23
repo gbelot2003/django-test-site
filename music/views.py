0311bdb2131a29b7910c5f4c6f9b39d71ff2fe62
gbelot <gbelot2003@hotmail.com>
2016-06-23 03:36:19 -0600

    refactory music.urls, views.detail and music.index.html

--- music/views.py
+++ music/views.py
@@ -1,16 +1,10 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
-
 
 def index(request):
 	all_albums = Album.objects.all()
 	return render(request, 'music/index.html', {'all_albums': all_albums})
 
 def detail(request, album_id):
-	try:
-		album = Album.objects.get(id=album_id)
-	except Album.DoesNotExist:
-		raise Http404("No existe ese album")
-
+	album = get_object_or_404(Album.objects.get(id=album_id))
 	return render(request, 'music/detail.html', {'album': album})
