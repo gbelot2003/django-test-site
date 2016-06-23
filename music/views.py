32a206ca5154f77bebc667cae55e9f5f21745d66
gbelot <gbelot2003@hotmail.com>
2016-06-23 00:47:31 -0600

    creating new view on music.views.py and updating urls.py to recive parameters

--- music/views.py
+++ music/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("<h2>This is the music App")
+
+def detail(request, album_id):
+	return HttpResponse("<h2>Details for Album id: " +  str(album_id) + "</h2>")
